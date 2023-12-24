ef5b9b0a2053dab5eefc8b1d5b885163934682bf
Rahulrockss <rahulskumar343@gmail.com>
2023-12-24 15:56:33 +0530

    Redux Setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,30 @@
 import { useState,useEffect } from 'react'
 import './App.css'
 import {fetchDataFromApi} from '../src/utils/api'
+import { useSelector, useDispatch } from 'react-redux'
+import { getApiConfiguration} from '../src/store/homeSlice'
 
 function App() {
-
+  const dispatch = useDispatch()
+  const {url}=useSelector((state)=>{
+    state.home
+  })
   useEffect(()=>{
     apiTesting();
   },[])
-  const apiTesting =()=>{
+const apiTesting =()=>{
     fetchDataFromApi('/movie/popular')
     .then((res)=>{
       console.log(res)
+      dispatch(getApiConfiguration(res))
     })
   }
-  
   return (
     <>
-<h1>Movie Vista </h1>
+        <div className="App"><h1>
+          {url?.total_pages}</h1>
+        </div>
     </>
   )
-}
-
+  }
 export default App
---
+++ src/components/header/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./style.sccs"
+
+function Footer() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Footer
